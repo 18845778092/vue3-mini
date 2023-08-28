af7d08034dd38d3b2f4703173ad9aefba63bcc84
xiaoli <2585960098@qq.com>
2023-08-29 01:28:50 +0800

    feat: support getCurrentInstance

---
+++ example/getCurrentInstance/Foo.js
@@ -0,0 +1,14 @@
+import { h, getCurrentInstance } from '../../lib/mini-vue.esm.js'
+
+const App = {
+  setup(props, { emit }) {
+    const instance = getCurrentInstance()
+    console.log('instance', instance)
+    return {}
+  },
+  render() {
+    const foo = h('p', {}, 'foo')
+    return h('div', {}, 'foo')
+  }
+}
+export default App
--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -5,6 +5,8 @@ import { initProps } from './componentProps'
 import { PublicInstanceProxyHandlers } from './componentPublicInstance'
 import { initSlots } from './componentSlots'
 
+let currentInstance = null
+
 export function createComponentInstance(vnode) {
   const component = {
     vnode,
@@ -29,9 +31,11 @@ function setupStatefulComponent(instance) {
   instance.proxy = new Proxy({ _: instance }, PublicInstanceProxyHandlers)
   const { setup } = Component
   if (setup) {
+    setCurrentInstance(instance)
     const setupResult = setup(shallowReadonly(instance.props), {
       emit: instance.emit
     })
+    setCurrentInstance(null)
     handleSetupResult(instance, setupResult)
   }
 }
@@ -50,4 +54,12 @@ function finishComponentSetup(instance) {
   if (!instance.render) {
     instance.render = Component.render
   }
+}
+
+export function getCurrentInstance() {
+  return currentInstance
+}
+
+export function setCurrentInstance(instance) {
+  currentInstance = instance
 }
