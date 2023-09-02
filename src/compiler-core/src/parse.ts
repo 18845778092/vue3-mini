03c7237cd16a6a05252e7cbe235a4cd9ebbf6fab
xiaoli <2585960098@qq.com>
2023-09-02 21:23:00 +0800

    feat: parse something...

---
+++ src/compiler-core/src/parse.ts
@@ -0,0 +1,83 @@
+import { NodeTypes } from './ast'
+const enum TagType {
+  START,
+  END
+}
+export function baseParse(content: string) {
+  const context = createParserContext(content)
+  return createRoot(parseChildren(context))
+}
+
+function parseChildren(context) {
+  const nodes: any = []
+  let node
+  const s = context.source
+  if (s.startsWith('{{')) {
+    node = parseInterpolation(context)
+  } else if (s[0] === '<') {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context)
+    }
+  }
+  nodes.push(node)
+  return nodes
+}
+
+function parseElement(context) {
+  const element = parseTag(context, TagType.START)
+  parseTag(context, TagType.END)
+  console.log('-------------', context)
+  return element
+}
+
+function parseTag(context, type) {
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source)
+  const tag = match[1]
+  advanceBy(context, match[0].length)
+  advanceBy(context, 1)
+  if (type === TagType.END) return
+
+  return {
+    type: NodeTypes.ELEMENT,
+    tag
+  }
+}
+
+// 解析插值表达式
+function parseInterpolation(context) {
+  const openDelimiter = '{{'
+  const closeDelimiter = '}}'
+  const closeIndex = context.source.indexOf(
+    closeDelimiter,
+    openDelimiter.length
+  )
+  advanceBy(context, openDelimiter.length)
+  const rawContentLength = closeIndex - openDelimiter.length
+  const rawContent = context.source.slice(0, rawContentLength)
+  const content = rawContent.trim()
+  advanceBy(context, rawContentLength + closeDelimiter.length)
+
+  return {
+    type: NodeTypes.INTERPOLATION,
+    content: {
+      type: NodeTypes.SIMPLE_EXPRESSION,
+      content: content
+    }
+  }
+}
+
+function advanceBy(context: any, length: number) {
+  context.source = context.source.slice(length)
+}
+
+function createRoot(children) {
+  return {
+    children
+  }
+}
+
+function createParserContext(content: string) {
+  return {
+    source: content
+  }
+}
