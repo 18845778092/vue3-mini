4ee29dfb3c2aa42d8e18e7090c1128d6e5d55f5e
xiaoli <2585960098@qq.com>
2023-09-03 18:24:29 +0800

    feat: transform和codegen逻辑

---
+++ src/compiler-core/src/transform.ts
@@ -0,0 +1,51 @@
+import { NodeTypes } from './ast'
+import { TO_DISPLAY_STRING } from './runtimeHelpers'
+
+export function transform(root, options = {}) {
+  const context = createTransformContext(root, options)
+  traverseNode(root, context)
+  createRootCodegen(root)
+  root.helpers = [...context.helpers.keys()]
+}
+
+function createRootCodegen(root) {
+  root.codegenNode = root.children[0]
+}
+
+function traverseNode(node: any, context) {
+  const nodeTransforms = context.nodeTransforms
+
+  for (let i = 0; i < nodeTransforms.length; i++) {
+    const transform = nodeTransforms[i]
+    transform(node)
+  }
+  switch (node.type) {
+    case NodeTypes.INTERPOLATION:
+      context.helper(TO_DISPLAY_STRING)
+      break
+    case NodeTypes.ROOT:
+    case NodeTypes.ELEMENT:
+      traverseChildren(node, context)
+      break
+  }
+}
+
+function traverseChildren(node, context) {
+  const children = node.children
+  for (let i = 0; i < children.length; i++) {
+    const node = children[i]
+    traverseNode(node, context)
+  }
+}
+
+function createTransformContext(root, options) {
+  const context = {
+    root,
+    nodeTransforms: options.nodeTransforms || [],
+    helpers: new Map(),
+    helper(key) {
+      context.helpers.set(key, 1)
+    }
+  }
+  return context
+}
